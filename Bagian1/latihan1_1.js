21e46243fdd6ddd1b1ca86cf2a8b1f4ea44c7bf1
sabar <sabarsimanjuntak03@students.unnes.ac.id>
2026-09-14 19:31:05 +0700

    latihan3

--- Bagian1/latihan1_1.js
+++
@@ -1,7 +0,0 @@
-let price = 1000;
-let discountpercent = 10
- // 10% discount
- function calculateDiscountedPrice(price, discountPercent) {
- return price - (price * discountPercent) / 100;
-}
-console.log(calculateDiscountedPrice(price, discountpercent));
